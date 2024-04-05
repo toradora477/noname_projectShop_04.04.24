01d5dcd7b35a6ec4c18f1ef708e24c8644696101
Dima Kryshtopa <toradora477@gmail.com>
2024-04-05 17:23:12 +0300

    no message

--- projects/react-app/src/App.js
+++ projects/react-app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import RouterSwitch from './RouterSwitch';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
+import MenuMain from './components/MenuMain';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <MenuMain />
         <RouterSwitch />
       </Router>
     </div>
