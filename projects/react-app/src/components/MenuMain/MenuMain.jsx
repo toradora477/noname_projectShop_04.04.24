694bfdbf453242a3332ac2ddfe318a03612c00d9
Dima Kryshtopa <toradora477@gmail.com>
2024-04-08 16:56:57 +0300

    no message

--- projects/react-app/src/components/MenuMain/MenuMain.jsx
+++ projects/react-app/src/components/MenuMain/MenuMain.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { ROUTES } from '../../common_constants/routes';
+
 import icon_logo_1 from '../../images/icon_logo_1.svg';
 import icon_loupe from '../../images/icon_loupe.svg';
-
 import './MenuMain.scss';
 
 const MenuMain = () => {
   return (
     <div className="menu-main">
-      <Link className="company-logo" to="/">
+      <Link className="company-logo" to={ROUTES.HOME_DASHBOARD}>
         <img src={icon_logo_1} alt="Company Logo" />
       </Link>
       <div className="links">
-        <Link className="links-item" to="/" children="Головна" />
-        <Link className="links-item" to="/shop" children="Магазин" />
-        <Link className="links-item" to="/top-prodazh-za-misyacz" children="Лідери Продажів" />
+        <Link className="links-item" to={ROUTES.HOME_DASHBOARD} children="Головна" />
+        <Link className="links-item" to={ROUTES.SHOP} children="Магазин" />
+        <Link className="links-item" to={ROUTES.TOP_PRODAZH_ZA_MISYACZ} children="Лідери Продажів" />
       </div>
       <div className="search-bar">
         <img className="loupe-icon" src={icon_loupe} alt="search magnifier" />
