d7390b85843c3402e06bce6635c7b8c0fe8eae38
Dima Kryshtopa <toradora477@gmail.com>
2024-04-08 20:46:13 +0300

    no message

--- projects/react-app/src/components/Product/Product.jsx
+++ projects/react-app/src/components/Product/Product.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Product.scss';
 
 const Product = ({ name, price, imageUrl }) => {
   return (
     <div className="product">
-      <img src={imageUrl} alt="image product" />
+      <img src={imageUrl} alt="product-image" />
       <h3>{name}</h3>
       <p>${price}</p>
       <button>Add to Cart</button>
