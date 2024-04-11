3e3f3248ca49f428a585306e7cf36b229e6ee17c
Dima Kryshtopa 2 <toradora477@gmail.com>
2024-04-11 15:41:08 +0300

    no message

---
+++ projects/react-app/src/components/Modal/Modal.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './Modal.scss';
+
+const Modal = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div>
+      <button onClick={openModal}>Open Modal</button>
+      {isOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
+            {children}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Modal;
