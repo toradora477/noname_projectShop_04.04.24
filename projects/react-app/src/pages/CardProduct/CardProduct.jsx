6e166895a01641aaa9df5c02048ac1f877adbcda
Dima Kryshtopa <toradora477@gmail.com>
2024-04-22 23:24:21 +0300

    no message

--- projects/react-app/src/pages/CardProduct/CardProduct.jsx
+++ projects/react-app/src/pages/CardProduct/CardProduct.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory, useParams } from 'react-router-dom';
 import ReactPaginate from 'react-paginate';
 import './CardProduct.scss';
 import { Product } from '../../components';
 
 const CardProduct = () => {
+  const [dispatch, history, { productId }] = [useDispatch(), useHistory(), useParams()];
+
   const products = useSelector((state) => state.common.products) ?? [];
   const [currentPage, setCurrentPage] = useState(0);
   const productsPerPage = 15; // Количество продуктов на странице
@@ -16,12 +19,23 @@ const CardProduct = () => {
     setCurrentPage(selected);
   };
 
+  const item = products.find((item) => item._id === productId);
+
+  console.log(item);
+
   const currentProducts = products.slice(offset, offset + productsPerPage);
 
   return (
     <div className="products-admin">
       <br />
       Інфо товару
+      <br />
+      {item._id}
+      <br />
+      {item.i} <br />
+      {item.imageUrl} <br />
+      {item.name} <br />
+      {item.price}
       <br />
     </div>
   );
