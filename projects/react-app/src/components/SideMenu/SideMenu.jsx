7f87f8f6e32fb1eed4ebfb7e9565fee56711e7ca
Dima Kryshtopa <toradora477@gmail.com>
2024-04-25 02:03:40 +0300

    no message

--- projects/react-app/src/components/SideMenu/SideMenu.jsx
+++ projects/react-app/src/components/SideMenu/SideMenu.jsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { icons8_t_shirt_96, icons8_shoes_96, icons8_pants_96, icons8_like_96, icons8_cap_96, icons8_bag_96, Icon_menu_sidemenu } from '../../images';
-
+import clsx from 'clsx';
 import './SideMenu.scss';
 
 const SideMenu = () => {
-  const ButtomSideMenu = ({ src }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const ButtomSideMenu = ({ src, text = 'верхній одяг' }) => {
     return (
       <button className="btn-first">
         <img src={src} alt="btn menu" />
+        {isExpanded && <span className="text">{text}</span>}
       </button>
     );
   };
 
   return (
-    <nav className="side-menu">
+    <nav
+      className={clsx('side-menu', {
+        expanded: isExpanded,
+      })}
+      onMouseEnter={() => setIsExpanded(true)}
+      onMouseLeave={() => setIsExpanded(false)}
+    >
       <div className="container">
         <br />
         <ButtomSideMenu src={Icon_menu_sidemenu} />
