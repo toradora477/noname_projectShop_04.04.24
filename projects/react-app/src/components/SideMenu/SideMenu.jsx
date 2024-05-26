55b0630f78381612fa22524d02fef916746b432c
Dima Kryshtopa <toradora477@gmail.com>
2024-05-27 01:37:42 +0300

    no message

--- projects/react-app/src/components/SideMenu/SideMenu.jsx
+++ projects/react-app/src/components/SideMenu/SideMenu.jsx
@@ -1,8 +1,31 @@
 import React, { useState, Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import { icons8_like_96, icons8_bag_96, Icon_menu_sidemenu } from '../../images';
 import { PRODUCT_CATEGORIES } from '../../common_constants/business';
 import clsx from 'clsx';
+import { ROUTES } from '../../common_constants/routes';
 import './SideMenu.scss';
+
+const ButtonWithIcon = ({ src, text, isExpanded, item, ml = 10, pathnameLink }) => {
+  return (
+    <Link
+      style={{ textDecoration: 'none' }}
+      to={{
+        pathname: pathnameLink ?? ROUTES.SHOP,
+        ...(pathnameLink ? {} : { search: `?category=${item?.value}` }),
+      }}
+    >
+      <button style={{ marginLeft: ml }} className="btn-first">
+        <img src={src ?? item?.img} alt="btn menu" />
+        {isExpanded && (
+          <span className="text" style={{ fontWeight: 700, fontSize: '16px' }}>
+            {text ?? item?.label}
+          </span>
+        )}
+      </button>
+    </Link>
+  );
+};
 
 const SideMenu = () => {
   const [isExpanded, setIsExpanded] = useState(false);
@@ -15,19 +38,10 @@ const SideMenu = () => {
     setIsExpanded(false);
   };
 
-  const ButtomSideMenu = ({ item, src, text, ml = 10 }) => {
-    return (
-      <button style={{ marginLeft: ml }} className="btn-first">
-        <img src={src ?? item?.img} alt="btn menu" />
-        {isExpanded && <span className="text">{text ?? item?.label}</span>}
-      </button>
-    );
-  };
-
   return (
     <Fragment>
       <div className="side-block" />
-      <div className={clsx('screen-dim', { visible: isExpanded })} />
+      <div className={clsx('side-screen-dim', { visible: isExpanded })} />
       <nav
         className={clsx('side-menu', {
           expanded: isExpanded,
@@ -37,12 +51,12 @@ const SideMenu = () => {
       >
         <div className="container">
           <br />
-          <ButtomSideMenu ml={0} src={Icon_menu_sidemenu} text="Меню" />
-          <ButtomSideMenu src={icons8_bag_96} text="Магазин" />
+          <ButtonWithIcon pathnameLink={ROUTES.HOME_DASHBOARD} ml={0} src={Icon_menu_sidemenu} text="Меню" isExpanded={isExpanded} />
+          <ButtonWithIcon pathnameLink={ROUTES.SHOP} src={icons8_bag_96} text="Магазин" isExpanded={isExpanded} />
           {PRODUCT_CATEGORIES.map((item, index) => (
-            <ButtomSideMenu item={item} key={index} />
+            <ButtonWithIcon item={item} key={index} isExpanded={isExpanded} />
           ))}
-          <ButtomSideMenu src={icons8_like_96} text="Лідери продажів" />
+          <ButtonWithIcon pathnameLink={ROUTES.SHOP} src={icons8_like_96} text="Лідери продажів" isExpanded={isExpanded} />
         </div>
       </nav>
     </Fragment>
