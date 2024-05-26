548fa3d68d412e6dc817ed6cc96a39d3b79163c2
Dima Kryshtopa <toradora477@gmail.com>
2024-05-27 00:07:24 +0300

    no message

--- projects/react-app/src/components/SideMenu/SideMenu.jsx
+++ projects/react-app/src/components/SideMenu/SideMenu.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-
+import React, { useState, Fragment } from 'react';
 import { icons8_like_96, icons8_bag_96, Icon_menu_sidemenu } from '../../images';
 import { PRODUCT_CATEGORIES } from '../../common_constants/business';
 import clsx from 'clsx';
@@ -7,9 +6,9 @@ import './SideMenu.scss';
 
 const SideMenu = () => {
   const [isExpanded, setIsExpanded] = useState(false);
-  const ButtomSideMenu = ({ item, src, text }) => {
+  const ButtomSideMenu = ({ item, src, text, ml = 10 }) => {
     return (
-      <button className="btn-first">
+      <button style={{ marginLeft: ml }} className="btn-first">
         <img src={src ?? item?.img} alt="btn menu" />
         {isExpanded && <span className="text">{text ?? item?.label}</span>}
       </button>
@@ -17,23 +16,26 @@ const SideMenu = () => {
   };
 
   return (
-    <nav
-      className={clsx('side-menu', {
-        expanded: isExpanded,
-      })}
-      onMouseEnter={() => setIsExpanded(true)}
-      onMouseLeave={() => setIsExpanded(false)}
-    >
-      <div className="container">
-        <br />
-        <ButtomSideMenu src={Icon_menu_sidemenu} text="Меню" />
-        <ButtomSideMenu src={icons8_bag_96} text="Магазин" />
-        {PRODUCT_CATEGORIES.map((item, index) => (
-          <ButtomSideMenu item={item} key={index} />
-        ))}
-        <ButtomSideMenu src={icons8_like_96} text="Лідери продажів" />
-      </div>
-    </nav>
+    <Fragment>
+      <div className="side-block" />
+      <nav
+        className={clsx('side-menu', {
+          expanded: isExpanded,
+        })}
+        onMouseEnter={() => setIsExpanded(true)}
+        onMouseLeave={() => setIsExpanded(false)}
+      >
+        <div className="container">
+          <br />
+          <ButtomSideMenu ml={0} src={Icon_menu_sidemenu} text="Меню" />
+          <ButtomSideMenu src={icons8_bag_96} text="Магазин" />
+          {PRODUCT_CATEGORIES.map((item, index) => (
+            <ButtomSideMenu item={item} key={index} />
+          ))}
+          <ButtomSideMenu src={icons8_like_96} text="Лідери продажів" />
+        </div>
+      </nav>
+    </Fragment>
   );
 };
 
